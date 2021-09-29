b4e749b94305a8b2ff64f7d956f7712376afd7cf
Jaykenn1997 <plancer104@gmail.com>
2021-09-29 20:59:38 +0800

    first commit

---
+++ src/app/participants/participants.service.ts
@@ -0,0 +1,15 @@
+export class ParticipantsService {
+    getParticipants(){
+        return [
+            {name: "justin poireir", age:40 + " years old", gender: "Male"},
+            {name: "Conor MCgregor", age:34 + " years old", gender: "Male"},
+            {name: "bryle mythtic", age:20 + " years old", gender: "Male"},
+            {name: "eddie Alvares", age:36 + " years old", gender: "Male"},
+            {name: "bong bong marcos", age:71 + " years old", gender: "Male"},
+            {name: "rodigo duterte", age:76 + " years old", gender: "Male"},
+            {name: "Mang tomas", age:47+ " years old", gender: "Male"},
+            {name: "neneng lamig", age:17 + " years old", gender: "Female"},
+        ];
+    }
+
+}
